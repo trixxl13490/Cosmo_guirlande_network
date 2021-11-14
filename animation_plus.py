1c86fafa6db23b2fa936202c7ab880ebef9e98c5
trixxl <berson.virgile@wanadoo.fr>
2021-11-14 20:00:59 +0100

    animation_plus fix

--- animation_plus.py
+++ animation_plus.py
@@ -27,6 +27,9 @@ def Fire( Cooling, Sparking, SpeedDelay): #int int int
     # Step 1.  Cool down every cell a little
     for i in range(num_pixel):
         cooldown = randrange(0, int(((Cooling * 10) / num_pixel) + 2))
+        print("i: ", i)
+        print("heat[i]: ", heat[i])
+        print("cooldown: ", cooldown)
 
     if (cooldown > heat[i]):
         heat[i] = 0
