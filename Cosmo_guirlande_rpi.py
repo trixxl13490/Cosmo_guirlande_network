fd471aba78ab7869981469981101e1b161a367eb
trixxl <berson.virgile@wanadoo.fr>
2021-11-16 00:19:13 +0100

    try to fix stop bug from rpi by starting & closing socket at every loop

--- Cosmo_guirlande_rpi.py
+++ Cosmo_guirlande_rpi.py
@@ -304,7 +304,7 @@ class Cosmo_guirlande_rpi(threading.Thread):
             while True:
 
                 # Create Socket to communicate
-                self.newSocket = Cosmo_Communication(self.guirlande_number, self.pixel_number, self.tcp_ip, args.tcp_port, self.buffer_size)
+                self.newSocket = Cosmo_Communication(self.guirlande_number, self.pixel_number, self.tcp_ip, self.tcp_port, self.buffer_size)
                 self.newSocket.start()
 
                 print("Cosmoguirlande class run")
