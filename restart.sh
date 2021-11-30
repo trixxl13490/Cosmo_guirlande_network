706f3026d018a0d2b0bf41285b0096df7404bc77
trixxl <berson.virgile@wanadoo.fr>
2021-11-30 21:46:33 +0100

    ajout fichier .sh pour restart à distance from ssh connection on GUI.py

--- restart.sh
+++ restart.sh
@@ -1,2 +1,2 @@
 sudo ps aux | grep gui_rpi.py | awk '{print $2}' | xargs sudo kill - 9
-sudo python3 gui_rpi.py 2 30 192.168.0.20 50002 1024
+sudo python3 /home/pi/Cosmo_guirlande_network/gui_rpi.py 2 30 192.168.0.20 50002 1024
