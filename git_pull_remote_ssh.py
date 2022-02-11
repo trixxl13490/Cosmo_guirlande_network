d95b6e4750ffa9251e71ceb2026fb3a73064ce9d
trixxl13490 <61876543+trixxl13490@users.noreply.github.com>
2022-02-11 22:44:16 +0100

    cartch socket.timeout

--- git_pull_remote_ssh.py
+++ git_pull_remote_ssh.py
@@ -15,39 +15,48 @@ h_name = socket.gethostname()
 IP_addres = socket.gethostbyname(h_name)
 
 #--------------------------------------------------------------------------------------------------------------------
+try:
+    #Create SSH connection with paramiko
+    ssh1 = paramiko.SSHClient()
+    ssh1.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh1.connect(hostname='192.168.0.5', username='pi', password='vbcgxb270694', timeout=5, port=22)
 
-#Create SSH connection with paramiko
-ssh1 = paramiko.SSHClient()
-ssh1.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh1.connect(hostname='192.168.0.5', username='pi', password='vbcgxb270694', timeout=5, port=22)
+    # kill GUI if running
+    ssh_stdin1, ssh_stdout1, ssh_stderr1 = ssh1.exec_command("cd /home/pi/Cosmo_guirlande_network/ && sudo git pull")
 
-# kill GUI if running
-ssh_stdin1, ssh_stdout1, ssh_stderr1 = ssh1.exec_command("cd /home/pi/Cosmo_guirlande_network/ && sudo git pull")
+    #print(stdout1.read())
+    print("ss1 passed")
 
-#print(stdout1.read())
-print("ss1 passed")
+except socket.timeout:
+    print("ss1 timeout")
+#----------------------------------------------------------------------------------------------------------------------
+try:
+    ssh2 = paramiko.SSHClient()
+    ssh2.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh2.connect(hostname='192.168.0.26', username='pi', password='vbcgxb270694', timeout=5, port=22)
 
+
+    # kill GUI if running
+
+    ssh_stdin2, ssh_stdout2, ssh_stderr2 = ssh2.exec_command("cd /home/pi/Cosmo_guirlande_network/ && sudo git pull")
+
+    print("ss2 passed")
+
+except socket.timeout:
+    print("ss2 timeout")
 
 #----------------------------------------------------------------------------------------------------------------------
-ssh2 = paramiko.SSHClient()
-ssh2.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh2.connect(hostname='192.168.0.26', username='pi', password='vbcgxb270694', timeout=5, port=22)
+try:
+    ssh3 = paramiko.SSHClient()
+    ssh3.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh3.connect(hostname='192.168.0.29', username='pi', password='vbcgxb270694', timeout=5, port=22)
 
 
-# kill GUI if running
+    # kill GUI if running
+    ssh_stdin3, ssh_stdout3, ssh_stderr3 = ssh3.exec_command("cd /home/pi/Cosmo_guirlande_network/ && sudo git pull")
 
-ssh_stdin2, ssh_stdout2, ssh_stderr2 = ssh2.exec_command("cd /home/pi/Cosmo_guirlande_network/ && sudo git pull")
+    print("ss3 passed")
+    time.sleep(2.5)
 
-print("ss2 passed")
-
-#----------------------------------------------------------------------------------------------------------------------
-ssh3 = paramiko.SSHClient()
-ssh3.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh3.connect(hostname='192.168.0.29', username='pi', password='vbcgxb270694', timeout=5, port=22)
-
-
-# kill GUI if running
-ssh_stdin3, ssh_stdout3, ssh_stderr3 = ssh3.exec_command("cd /home/pi/Cosmo_guirlande_network/ && sudo git pull")
-
-print("ss3 passed")
-time.sleep(2.5)
+except socket.timeout:
+    print("ss3 timeout")
