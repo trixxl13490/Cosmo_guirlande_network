b3ee3aaf61d10aab267a8011755753320617cef5
trixxl13490 <61876543+trixxl13490@users.noreply.github.com>
2022-08-06 16:46:28 +0200

    Update Cosmo_guirlande_rpi.py

--- Cosmo_guirlande_rpi.py
+++ Cosmo_guirlande_rpi.py
@@ -75,7 +75,7 @@ class Cosmo_Communication(threading.Thread):
 
                 ##fermeture connexion
                 connexion_serveur.close()
-                # time.sleep(0.5) 
+                time.sleep(0.05)
 
                 if self.data_rcv.find('stop_dancingPi') != -1:
                     print("close dancyPi from cosmo_communication class")
