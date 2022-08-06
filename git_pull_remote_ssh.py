417a06aed1cb6a8e4152a60483d52ab58dd794b4
trixxl13490 <61876543+trixxl13490@users.noreply.github.com>
2022-08-06 16:06:07 +0200

    add MQTT test scipt

--- git_pull_remote_ssh.py
+++ git_pull_remote_ssh.py
@@ -33,7 +33,7 @@ except socket.timeout:
 try:
     ssh2 = paramiko.SSHClient()
     ssh2.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh2.connect(hostname='192.168.0.26', username='pi', password='vbcgxb270694', timeout=5, port=22)
+    ssh2.connect(hostname='192.168.0.7', username='pi', password='vbcgxb270694', timeout=5, port=22)
 
 
     # kill GUI if running
@@ -49,7 +49,7 @@ except socket.timeout:
 try:
     ssh3 = paramiko.SSHClient()
     ssh3.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh3.connect(hostname='192.168.0.29', username='pi', password='vbcgxb270694', timeout=5, port=22)
+    ssh3.connect(hostname='192.168.0.36', username='pi', password='vbcgxb270694', timeout=5, port=22)
 
 
     # kill GUI if running
@@ -60,3 +60,19 @@ try:
 
 except socket.timeout:
     print("ss3 timeout")
+
+#----------------------------------------------------------------------------------------------------------------------
+try:
+    ssh4 = paramiko.SSHClient()
+    ssh4.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh4.connect(hostname='192.168.0.36', username='pi', password='vbcgxb270694', timeout=5, port=22)
+
+
+    # kill GUI if running
+    ssh_stdin3, ssh_stdout3, ssh_stderr3 = ssh4.exec_command("cd /home/pi/Cosmo_guirlande_network/ && sudo git pull")
+
+    print("ss4 passed")
+    time.sleep(2.5)
+
+except socket.timeout:
+    print("ss4 timeout")
