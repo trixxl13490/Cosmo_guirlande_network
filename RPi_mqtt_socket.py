ff6d8435cf75cffc449e8a322b8a99b956c8cd25
trixxl13490 <61876543+trixxl13490@users.noreply.github.com>
2022-08-07 13:02:43 +0200

    Update RPi_mqtt_socket.py

--- RPi_mqtt_socket.py
+++ RPi_mqtt_socket.py
@@ -4,6 +4,8 @@ import threading
 #Don't forget to install "sudo apt-get install -y mosquitto mosquitto-clients"
 
 class RPi_mqtt_socket(threading.Thread):
+    cosmoguirlande=mqtt.Client()
+    
     def __init__(self):
         threading.Thread.__init__(self)
         self.data_rcv = ""
@@ -16,7 +18,7 @@ class RPi_mqtt_socket(threading.Thread):
         print(str(msg))
         self.data_rcv = str(msg)
 
-    cosmoguirlande=mqtt.Client()
+    
     cosmoguirlande.connect("localhost",1883,60)
     cosmoguirlande.on_connect = on_connect
     cosmoguirlande.on_message = on_message
