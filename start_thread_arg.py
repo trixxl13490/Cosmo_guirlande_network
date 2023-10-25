549dd65ef11ea3d1a0954add7a256ad3b0f93bb1
DESKTOP-FL72F2A\PC <berson.virgile@wanadoo.fr>
2023-10-25 11:32:32 +0200

    cleaning + killing subprocess failing stops script - try to assert it with try except finally

--- start_thread_arg.py
+++ start_thread_arg.py
@@ -39,133 +39,72 @@ class Thread_start_display_MAC(threading.Thread):
 
   def run(self):
       try:
-        """ssh1 = paramiko.SSHClient()
-        ssh1.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh1.connect(hostname=self.ip, username='pi', password='vbcgxb270694', timeout=5, port=22)
+        subprocess.Popen(args = "sudo ps aux | grep gui_rpi.py | awk '{print $2}' | xargs sudo kill -9", shell=True)
+        print("former term killed")
 
-        #time.sleep(2)
-
-        # kill GUI if running
-        ssh_stdin, ssh_stdout, ssh_stderr = ssh1.exec_command("sudo ps aux | grep gui_rpi.py | awk '{print $2}' | xargs sudo kill -9")
-        #time.sleep(1)
-
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(hostname=self.ip, username='pi', password='vbcgxb270694', timeout=5, port=22)
-
-        # Start GUI again on rpi
-        channel = ssh1.invoke_shell()
-        stdin = channel.makefile('wb')
-        stdout = channel.makefile('rb')"""
-        
-        """try:
-          subprocess.Popen(args = "sudo ps aux | grep gui_rpi.py | awk '{print $2}' | xargs sudo kill -9", shell=True)
-          print("former term killed")
-        except:
-           print("no active guirlande python term")"""
-
+      except socket.timeout:
+          print("ss1 timeout")
+          
+      except:
+          print("no active guirlande python term")
+      finally:
+          
         print("mac = ", self.mac)
 
         #----------------------------------------------case 1 : config RGBW 144 LEDs
         if self.mac == "74:da:38:f6:dd:ef":
-            """ssh.exec_command('''
-              export XAUTHORITY=/home/pi/.Xauthority
-              DISPLAY=:0  /usr/bin/lxterm -e 'sudo python3 /home/pi/Cosmo_guirlande_network/gui_rpi.py 1 42 192.168.0.20 50001 1024 RGB'
-              ''')"""
-            
             subprocess.Popen(args='''
               export XAUTHORITY=/home/pi/.Xauthority
               DISPLAY=:0  /usr/bin/lxterm -e 'sudo python3 /home/pi/Cosmo_guirlande_network/gui_rpi.py 1 42 192.168.0.20 50001 1024 RGB'
               ''', shell=True)
         #----------------------------------------------case 2 : config RGBW 144 LEDs
         elif self.mac == "b8:27:eb:04:77:43":
-            """ssh.exec_command('''
-              export XAUTHORITY=/home/pi/.Xauthority
-              DISPLAY=:0  /usr/bin/lxterm -e 'sudo python3 /home/pi/Cosmo_guirlande_network/gui_rpi.py 1 144 192.168.0.20 50001 1024 RGBW'
-              ''')"""
-            
             subprocess.Popen(args='''
               export XAUTHORITY=/home/pi/.Xauthority
               DISPLAY=:0  /usr/bin/lxterm -e 'sudo python3 /home/pi/Cosmo_guirlande_network/gui_rpi.py 1 144 192.168.0.20 50001 1024 RGBW'
               ''', shell=True)
         #----------------------------------------------case 3 : config RGBW 144 LEDs
         elif self.mac == "b8:27:eb:5d:3d:de":
-            """ssh.exec_command('''
-              export XAUTHORITY=/home/pi/.Xauthority
-              DISPLAY=:0  /usr/bin/lxterm -e 'sudo python3 /home/pi/Cosmo_guirlande_network/gui_rpi.py 1 144 192.168.0.20 50001 1024 RGBW'
-              ''')"""
-            
             subprocess.Popen(args='''
               export XAUTHORITY=/home/pi/.Xauthority
               DISPLAY=:0  /usr/bin/lxterm -e 'sudo python3 /home/pi/Cosmo_guirlande_network/gui_rpi.py 1 144 192.168.0.20 50001 1024 RGBW'
               ''', shell=True)
         #----------------------------------------------case 4 : config RGBW 144 LEDs
         elif self.mac == "b8:27:eb:a4:fd:a8":
-            """ssh.exec_command('''
-              export XAUTHORITY=/home/pi/.Xauthority
-              DISPLAY=:0  /usr/bin/lxterm -e 'sudo python3 /home/pi/Cosmo_guirlande_network/gui_rpi.py 1 30 192.168.0.20 50001 1024 RGBW'
-              ''')"""  
-                  
             subprocess.Popen(args='''
               export XAUTHORITY=/home/pi/.Xauthority
               DISPLAY=:0  /usr/bin/lxterm -e 'sudo python3 /home/pi/Cosmo_guirlande_network/gui_rpi.py 1 30 192.168.0.20 50001 1024 RGBW'
               ''', shell=True)
         #----------------------------------------------case 5 : config RGBW 144 LEDs
         elif self.mac == "b8:27:eb:bf:ac:3a":
-            """ssh.exec_command('''
-              export XAUTHORITY=/home/pi/.Xauthority
-              DISPLAY=:0  /usr/bin/lxterm -e 'sudo python3 /home/pi/Cosmo_guirlande_network/gui_rpi.py 1 144 192.168.0.20 50001 1024 RGBW'
-              ''')"""
-            
             subprocess.Popen(args='''
               export XAUTHORITY=/home/pi/.Xauthority
               DISPLAY=:0  /usr/bin/lxterm -e 'sudo python3 /home/pi/Cosmo_guirlande_network/gui_rpi.py 1 144 192.168.0.20 50001 1024 RGBW'
               ''', shell=True)
         #----------------------------------------------case 6 : config RGBW 144 LEDs
         elif self.mac == "b8:27:eb:c2:e9:2b":
-            """ssh.exec_command('''
-              export XAUTHORITY=/home/pi/.Xauthority
-              DISPLAY=:0  /usr/bin/lxterm -e 'sudo python3 /home/pi/Cosmo_guirlande_network/gui_rpi.py 1 35 192.168.0.20 50001 1024 RGB'
-              ''')   """  
-           
             subprocess.Popen(args='''
               export XAUTHORITY=/home/pi/.Xauthority
               DISPLAY=:0  /usr/bin/lxterm -e 'sudo python3 /home/pi/Cosmo_guirlande_network/gui_rpi.py 1 35 192.168.0.20 50001 1024 RGB'
               ''', shell=True)
         #----------------------------------------------case 7 : config RGBW 144 LEDs
         elif self.mac == "b8:27:eb:d0:01:ff":
-            """ssh.exec_command('''
-              export XAUTHORITY=/home/pi/.Xauthority
-              DISPLAY=:0  /usr/bin/lxterm -e 'sudo python3 /home/pi/Cosmo_guirlande_network/gui_rpi.py 1 50 192.168.0.20 50001 1024 RGBW'
-              ''')"""
-            
             subprocess.Popen(args='''
               export XAUTHORITY=/home/pi/.Xauthority
               DISPLAY=:0  /usr/bin/lxterm -e 'sudo python3 /home/pi/Cosmo_guirlande_network/gui_rpi.py 1 50 192.168.0.20 50001 1024 RGBW'
               ''', shell=True)
         #----------------------------------------------case 8 : config RGBW 144 LEDs
         elif self.mac == "b8:27:eb:ed:90:54":
-            """ssh.exec_command('''
-              export XAUTHORITY=/home/pi/.Xauthority
-              DISPLAY=:0  /usr/bin/lxterm -e 'sudo python3 /home/pi/Cosmo_guirlande_network/gui_rpi.py 1 144 192.168.0.20 50001 1024 RGBW'
-              ''')"""
-            
             subprocess.Popen(args='''
               export XAUTHORITY=/home/pi/.Xauthority
               DISPLAY=:0  /usr/bin/lxterm -e 'sudo python3 /home/pi/Cosmo_guirlande_network/gui_rpi.py 1 144 192.168.0.20 50001 1024 RGBW'
               ''', shell=True)
         #----------------------------------------------case 9 : config RGBW 144 LEDnot known
         else:
-           print("no matching MAC, basic config 144 RGBW")
+          print("no matching MAC, basic config 144 RGBW")
         #----------------------------------------------
         print("ssh passed : ", self.mac)
 
-        """print('stdout.read()' , repr(stdout.read()))
-        stdout.close()
-        stdin.close()"""
-
-      except socket.timeout:
-          print("ss1 timeout")
 
 
 if __name__ == "__main__":
